634fcdac851ae8df256f3ec6d0d5a65da2f7beea
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-06-15 16:35:32 +0200

    And add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -16,12 +16,18 @@ from wikidataeditor.item import Item
 class TestClient(unittest.TestCase):
 
     def setUp(self):
-        ua = 'MyTool/0.1'
-        site = Site(ua)
-        self.item = Item(site, 'Q1000')
+        self.ua = 'MyTool/0.1'
+        self.site = Site(self.ua)
+        self.item = Item(self.site, 'Q1000')
 
-    def test_initialization(self):
-        assert self.item.entity == '1000'
+    def test_entity_id_normalization(self):
+        item1 = Item(self.site, 'Q1000')
+        item2 = Item(self.site, '1000')
+        item3 = Item(self.site, 1000)
+
+        assert item1.entity == 'Q1000'
+        assert item2.entity == 'Q1000'
+        assert item3.entity == 'Q1000'
 
 
 if __name__ == '__main__':
